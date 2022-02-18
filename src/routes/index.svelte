6bda5f19e20b8fd3501260e28587e1bdadd748b7
Byronn Walker-Mills <bwalkermills@gmail.com>
2022-02-18 19:33:05 +0000

    Add styling for mobile

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+  <p>Advice #117</p>
+  <h2>&ldquo;It is easy to sit up and take notice, what&lsquo;s difficult is getting up and taking action.&rdquo;</h2>
+
+  <picture>
+    <source srcset="/pattern-divider-desktop.svg"
+            media="(min-width: 992px)">
+    <img src="/pattern-divider-mobile.svg" alt="" />
+  </picture>
+
+  <button>
+    <span class="sr-only">New advice quote...</span>
+    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>
+  </button>
+</div>
